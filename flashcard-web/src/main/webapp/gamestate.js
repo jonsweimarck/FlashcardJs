7df34ec61defeba2d8b81bcb89a3166c743ae247
jonsweimarck <jonsweimarck@gmail.com>
2014-09-24 21:35:21 +0200

    All logik fungerar, inklusive anrop till graf

--- flashcard-web/src/main/webapp/gamestate.js
+++ flashcard-web/src/main/webapp/gamestate.js
@@ -6,66 +6,116 @@ var Gamestate = (function () {
 
     var cards;
     var cardIndex;
-    var missedCards =[];
+    var missedCardsFirstRound =[];
     var readyForNextCard;
+    var isFirstRound;
 
     var STATE_CARD_NOT_YET_SHOWN = 0;
     var STATE_CARD_OK_FIRST_ROUND = 1;
     var STATE_CARD_OK_OTHER_ROUND = 2;
     var STATE_CARD_MISSED = 3;
 
-    var init = function(cardsToUse, readyForNextCardCallback){
-        console.log("Gamestate init med " + cardsToUse.length + " kort");
+    var initFirstRound = function(cardsToUse, readyForNextCardCallback){
+        console.log("Gamestate initFirstRound med " + cardsToUse.length + " kort");
         cards = addInitStateToCards(cardsToUse);
         cardIndex = -1;
-        missedCards =[]; // Viktigt eftersom init anropas även för att köra igång missedCardRound
+        missedCardsFirstRound =[];
         readyForNextCard = readyForNextCardCallback;
+        isFirstRound = true;
+    }
+
+    var initOtherRound = function(){
+        console.log("Gamestate initOtherRound ");
+        cardIndex = -1;
+        isFirstRound = false;
     }
 
     function addInitStateToCards(cards) {
         for (var i = 0; i < cards.length; i++){
             cards[i].state = STATE_CARD_NOT_YET_SHOWN;
         }
+        return cards;
+    }
+
+    var nbrCardsOkFirstRound = function(){return nbrWithState(STATE_CARD_OK_FIRST_ROUND);}
+
+    var nbrCardsOkOtherRounds = function(){return nbrWithState(STATE_CARD_OK_OTHER_ROUND);}
+
+    var nbrCardsMissed = function(){return nbrWithState(STATE_CARD_MISSED);}
+
+    var nbrCardsleft =  function(){return nbrWithState(STATE_CARD_NOT_YET_SHOWN);}
+
+    function nbrWithState(stateToCount){
+        var count = 0;
+        for (var i = 0; i < cards.length; i++) {
+            if(cards[i].state === stateToCount){
+                count ++;
+            }
+        }
+        return count;
     }
 
     var hasMoreCards = function(){
-        return cardIndex < cards.length - 1;
+        for (var i = cardIndex + 1; i < cards.length; i++) {
+            if(cards[i].state === STATE_CARD_MISSED || cards[i].state === STATE_CARD_NOT_YET_SHOWN){
+                return true;
+            }
+        }
+        return false;
     }
 
     var nextCard = function(){
         console.log('nextCard');
-        cardIndex ++;
-        return cards[cardIndex];
+        for (cardIndex ++; cardIndex < cards.length; cardIndex++) {
+            if(cards[cardIndex].state === STATE_CARD_MISSED || cards[cardIndex].state === STATE_CARD_NOT_YET_SHOWN){
+                return cards[cardIndex];
+            }
+        }
+        return null;
     }
 
     var currentCardOk = function(){
         console.log('currentCardOk');
+        if(isFirstRound){
+            cards[cardIndex].state = STATE_CARD_OK_FIRST_ROUND;
+        } else {
+            cards[cardIndex].state = STATE_CARD_OK_OTHER_ROUND;
+        }
+
         readyForNextCard();
     }
 
     var currentCardNok = function(){
         console.log('currentCardNok');
-        missedCards.push(cards[cardIndex]);
-        console.log('cardsNok added, length: ' + missedCards.length);
+        cards[cardIndex].state = STATE_CARD_MISSED;
+        if(isFirstRound){
+            missedCardsFirstRound.push(cards[cardIndex]);
+            console.log('cardsNok added, length: ' + missedCardsFirstRound.length);
+        }
         readyForNextCard();
     }
 
     var missedCardsExists = function(){
-        return missedCards.length > 0;
+        return nbrCardsMissed() > 0;
     }
 
-    var getMissedCard = function(){
-        return missedCards;
+    var getMissedCardFirstRound = function(){
+        return missedCardsFirstRound;
     }
 
     return {
-        init: init,
+        initFirstRound: initFirstRound,
+        initOtherRound: initOtherRound,
         hasMoreCards: hasMoreCards,
         nextCard: nextCard,
         currentCardOk : currentCardOk,
         currentCardNok : currentCardNok,
         missedCardsExists : missedCardsExists,
-        getMissedCard : getMissedCard
+        getMissedCardFirstRound : getMissedCardFirstRound,
+        nbrCardsOkFirstRound : nbrCardsOkFirstRound,
+        nbrCardsOkOtherRounds :nbrCardsOkOtherRounds,
+        nbrCardsMissed :nbrCardsMissed,
+        nbrCardsleft : nbrCardsleft
     };
 
 })();
