5198b64de2a83da0010aa87ae658fd9c622c3049
jonsweimarck <jonsweimarck@gmail.com>
2014-09-28 16:39:21 +0200

    kopierat i grafen fån flashcard

--- flashcard-web/src/main/webapp/js/gui.js
+++ flashcard-web/src/main/webapp/js/gui.js
@@ -28,10 +28,12 @@ var Gui = (function () {
 
     var showUpdatedGraph = function(okFirstRound, okOtherRounds, missed, left){
         $("#showgraph_div").text(
-                " okFirstRound: " + okFirstRound +
+                " okFirstRound: "   + okFirstRound +
                 ", okOtherRounds: " + okOtherRounds +
-                ", missed: " + missed +
-                ", left: " + left);
+                ", missed: "        + missed +
+                ", left: "          + left);
+
+        drawChart(okFirstRound, okOtherRounds, missed, left);
     }
     var showFinished = function(cardsToshow){
         $("#showcard_div").hide();
@@ -43,8 +45,26 @@ var Gui = (function () {
             missedText += cardsToshow[i].q + " ";
         }
         $("#finished_text").text(missedText);
+    }
 
+    function drawChart(okFirstRound, okOtherRounds, missed, left) {
+        var data = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Klarade direkt',              okFirstRound],
+            ['Klarade efter flera försök',  okOtherRounds],
+            ['Fel',  		                missed],
+            ['Kvar',     	                left],
+        ]);
 
+        var options = {
+            colors:['limegreen','green','red','blue'],
+            backgroundColor: '#D0E7EF',
+            chartArea:{left:8,top:10,width:"84%",height:"84%"},
+            legend:{position: 'none'}, reverseCategories:false
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
     }
 
     return {
