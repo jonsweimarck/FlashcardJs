02886073a6e2c0b2380a9104dc331fa27d71feb7
jonsweimarck <jonsweimarck@gmail.com>
2014-10-01 19:54:38 +0200

    Visar kortlekarna

--- flashcard-web/src/main/webapp/js/decks.js
+++ flashcard-web/src/main/webapp/js/decks.js
@@ -16,8 +16,20 @@ var Decks = (function () {
 
     };
 
-    var getAllDecks = function(){
-        return allDecks;
+    var getAddDecks = function(){
+        return [deck1, deck2];
+    }
+
+    var getSubDecks = function(){
+        return [deck2, deck1];
+    }
+
+    var getMultiDecks = function(){
+        return [deck1];
+    }
+
+    var getHomeworkDecks = function(){
+        return [deck1, deck2];
     }
 
     /**
@@ -43,18 +55,39 @@ var Decks = (function () {
 
     var deck1 =
         {
-            id : 1,
-            desc: 'Kortlek nummer 1',
-            cards : cards1
+            id      : 1,
+            name    : 'Kortleksnamn 1',
+            desc    : 'Kortlek nummer 1',
+            cards   : [
+                        {q:"1 + 1 = ", a:"1 + 1 = 2"},
+                        {q:"1 + 2 = ", a:"1 + 2 = 3"},
+                        {q:"1 + 3 = ", a:"1 + 3 = 4"}
+                       ]
         };
 
-    var allDecks = [deck1];
+    var deck2 =
+    {
+        id      : 1,
+        name    : 'Kortleksnamn 2',
+        desc    : 'Kortlek nummer 2',
+        cards   : [
+            {q:"1 - 1 = ", a:"1 - 1 = 0"},
+            {q:"2 - 2 = ", a:"1 - 2 = 0"},
+            {q:"3 - 3 = ", a:"1 - 3 = 0"}
+        ]
+    };
+
+
+
 
 
 
     return {
-        getDeck: getDeck,
-        getAllDecks: getAllDecks
+        getDeck         : getDeck,
+        getAddDecks     : getAddDecks,
+        getSubDecks     : getSubDecks,
+        getMultiDecks   : getMultiDecks,
+        getHomeworkDecks: getHomeworkDecks
     };
 
 })();
